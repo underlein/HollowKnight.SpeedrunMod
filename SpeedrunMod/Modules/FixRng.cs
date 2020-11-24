e491661b50e6cbd69f6001aba1672a253ff646dd
underlein <nicholaskamlah@gmx.de>
2020-11-24 06:24:00 +0100

    consistent kp vengeflies, consistent abyss bridge cdash, make furious vengeflies trigger later

--- SpeedrunMod/Modules/FixRng.cs
+++ SpeedrunMod/Modules/FixRng.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections;
+using HutongGames.PlayMaker.Actions;
 using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 using Vasi;
 using USceneManager = UnityEngine.SceneManagement.SceneManager;
 
@@ -11,10 +14,12 @@ namespace SpeedrunMod.Modules {
             Unload();
 
             On.PlayMakerFSM.OnEnable += ModifyFsm;
+            USceneManager.activeSceneChanged += SceneChanged;
         }
 
         public override void Unload() {
             On.PlayMakerFSM.OnEnable -= ModifyFsm;
+            USceneManager.activeSceneChanged -= SceneChanged;
         }
 
         private static void ModifyFsm(On.PlayMakerFSM.orig_OnEnable orig, PlayMakerFSM self) {
@@ -33,6 +38,11 @@ namespace SpeedrunMod.Modules {
                 case "Shade Control" when self.name.StartsWith("Hollow Shade"): {
                     self.GetState("Init").RemoveAction(6);
                     self.FsmVariables.FindFsmInt("SP").Value = 0;
+                    break;
+                }
+                // make furious vengeflies trigger later
+                case "Control" when self.name.StartsWith("Angry Buzzer"): {
+                    self.gameObject.GetComponentInChildren<AlertRange>().transform.localScale = new Vector3(10, 10, 1.3f);
                     break;
                 }
             }
@@ -55,6 +65,44 @@ namespace SpeedrunMod.Modules {
                 "Fly 10" => 0,
                 _ => 0
             };
+        }
+
+        private static void SceneChanged(Scene from, Scene to) {
+            switch (to.name) {
+                case "Abyss_02": {
+                    HeroController.instance.StartCoroutine(Abyss_02());
+                    break;
+                }
+                case "Tutorial_01": {
+                    HeroController.instance.StartCoroutine(Tutorial_01());
+                    break;
+                }
+            }
+        }
+
+        private static IEnumerator Abyss_02() {
+            yield return null;
+
+            string[] names = {"Ruins Flying Sentry", "Ruins Flying Sentry Javelin", "Ruins Flying Sentry (1)"};
+
+            foreach (string name in names) {
+                RemoveIdleBuzzSpeed(GameObject.Find(name).LocateMyFSM("Flying Sentry " + (name.EndsWith("Javelin") ? "Javelin" : "Nail")).GetState("Idle Buzz").GetAction<IdleBuzz>());
+            }
+        }
+
+        private static IEnumerator Tutorial_01() {
+            yield return null;
+
+            GameObject buzzer = GameObject.Find("Buzzer");
+            RemoveIdleBuzzSpeed(buzzer.LocateMyFSM("chaser").GetState("Idle").GetAction<IdleBuzz>());
+            buzzer.transform.position = new Vector3(88, 32.4f);
+            
+            RemoveIdleBuzzSpeed(GameObject.Find("Buzzer 1").LocateMyFSM("chaser").GetState("Idle").GetAction<IdleBuzz>());
+        }
+
+        private static void RemoveIdleBuzzSpeed(IdleBuzz idleBuzz) {
+            idleBuzz.speedMax = 0;
+            idleBuzz.accelerationMax = 0;
         }
 
     }
